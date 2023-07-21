f1a042667948295d1e32e6e39f06f948bb041074
ptfn <CipherDogs@protonmail.com>
2023-07-21 11:31:05 +0700

    fixed and updated

--- i3/light.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-a=$(brightnessctl i | grep Curren)
-a=${a#*(}
-a=${a%'%'*}
-brightnessctl set $(expr $a $1 10)%
--- polybar/brightness.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-a=$(brightnessctl i | grep Curren)
-a=${a#*(}
-a=${a%'%'*}
-echo -e " $a%"
